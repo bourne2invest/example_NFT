35f5cd8209002b6df0fac8232735e5733c7d3440
jfm5344 <jfm6279@gmail.com>
2022-03-25 15:09:08 -0500

    create contract, add counter

--- contracts/ExampleNFT.sol
+++ contracts/ExampleNFT.sol
@@ -7,3 +7,8 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract ExampleNFT is ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+}

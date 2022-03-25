06c28fb26eaea50853972ddd62472a9e92bbcaca
jfm5344 <jfm6279@gmail.com>
2022-03-25 12:42:42 -0500

    import ERC721, Counters, Ownable, URIStorage classes

---
+++ contracts/ExampleNFT.sol
@@ -0,0 +1,9 @@
+//Contract based on [https://docs.openzeppelin.com/contracts/3.x/erc721](https://docs.openzeppelin.com/contracts/3.x/erc721)
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0; //set solidity version
+
+// inherit OpenZeppelin smart contract classes
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
